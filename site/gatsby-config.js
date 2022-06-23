c8a4cdc77c4ff1f87cc6badd4cc61d49828d6f99
Jeremy Collins <jeremy@jeremycollins.net>
2022-06-23 09:32:40 -0700

    metadata update

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -73,9 +73,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `JeremyCollins.net`,
+        short_name: `JeremyCollins.net`,
+        description: `Jeremy Collins' web presence`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
